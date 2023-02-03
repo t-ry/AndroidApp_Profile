73b65bf6a7ad1d1b8e09671be531620e52c135ac
takahashi.r <takahashi.r299792458@gmail.com>
2023-02-04 06:41:45 +0900

    change: profile_content

--- app/src/main/java/app/takahashi/kamesan/profile/MainActivity.kt
+++ app/src/main/java/app/takahashi/kamesan/profile/MainActivity.kt
@@ -15,22 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding.showProfileButton.setOnClickListener{
             binding.profileImage.setImageResource( R.drawable.randy_image )
             binding.profileComentText.text = "名前"
-            binding.profileComentText.text = "Androidメンターのランディだよ"
+            binding.profileComentText.text = "かめさんって呼ばれてるよ\nKotlinとJSをぼちぼちと触ってる\nHAREの服とファミマがすき"
         }
         binding.showSportButton.setOnClickListener {
             binding.profileImage.setImageResource(R.drawable.baseball_image)
             binding.profileLabelText.text = "スポーツ"
-            binding.profileComentText.text = "野球観戦が好きで、スタジアムに通っている"
+            binding.profileComentText.text = "実は空手を11年やってました"
         }
         binding.showFoodButton.setOnClickListener {
             binding.profileImage.setImageResource(R.drawable.donut_image)
             binding.profileLabelText.text = "好きな食べ物"
-            binding.profileComentText.text = "キャンディやドーナツが大好き"
+            binding.profileComentText.text = "おすしとすいーつ、チョコレートはビターがすき"
         }
         binding.showHobbyButton.setOnClickListener {
             binding.profileImage.setImageResource(R.drawable.gadget_image)
             binding.profileLabelText.text = "趣味"
-            binding.profileComentText.text = "ガジェットを集めて動かすこと"
+            binding.profileComentText.text = "広く浅く、何でもやります。\n最近はValorantと料理にはまってるよ"
         }
     }
 }
